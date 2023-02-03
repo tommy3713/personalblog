3a68e3e8788c2d660094559e1e91becbece6db4b
tommy3713 <tommy3713@gmail.com>
2023-02-03 11:39:28 -0800

    feat: add routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MenuComponent } from './menu/menu.component';
 import { TopbannerComponent } from './topbanner/topbanner.component';
 import { TimelineComponent } from './timeline/timeline.component';
-
+import { RouterModule } from '@angular/router';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ArticlesPageComponent } from './articles-page/articles-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TopbannerComponent,
-    TimelineComponent
+    TimelineComponent,
+    ProjectsPageComponent,
+    HomePageComponent,
+    ArticlesPageComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomePageComponent},
+      {path: 'projects', component: ProjectsPageComponent},
+      {path: 'articles', component: ArticlesPageComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
